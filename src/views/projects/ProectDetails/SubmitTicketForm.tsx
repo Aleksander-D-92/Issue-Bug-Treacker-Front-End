b8de3bdef672ac8c8e4ff1e514b30f1a59fc3a24
Aleksander D <aleksander.dorkov@gmail.com>
2020-08-19 22:07:35 +0300

    Committing before going to bed
    Minor formatting changes, fixed name

--- src/views/projects/ProectDetails/SubmitTicketForm.tsx
+++ src/views/projects/ProectDetails/SubmitTicketForm.tsx
@@ -13,48 +13,42 @@ function SubmitTicketForm() {
     const {projectId} = useParams();
     const userId = state.userDetails.id;
     useEffect(() => {
-        // axios.get('', {
-        //     headers: {Authorization: state.userDetails.authorizationHeader}
-        // }).then((e) => {
-        //     console.log(e);
-        // })
     }, [])
 
-    function onFinish(data: any) {
+    function onFinish(formData: any) {
+        const data = {
+            projectId: projectId,
+            submitterId: userId,
+            title: formData.title,
+            description: formData.description,
+            category: formData.category,
+            priority: formData.priority
+        }
+        axios.post('/tickets/submit-ticket', data, {
+            headers: {Authorization: state.userDetails.authorizationHeader}
+        })
         console.log(data);
     }
 
     return (
         <React.Fragment>
             <h1>Submit a ticket</h1>
-            <Form
-                name="basic"
-                initialValues={{remember: true}}
-                layout={'vertical'}
-                onFinish={onFinish}
-            >
-                <Form.Item
-                    label="Username"
-                    name="username"
-                    validateTrigger={false}
-                    rules={[{required: true, min: 5, max: 12, message: 'required: true, min: 5, max: 12'}]}
+            <Form name="basic" initialValues={{remember: true}} layout={'vertical'} onFinish={onFinish}>
+
+                <Form.Item label="Title" name="title" validateTrigger={false}
+                           rules={[{required: true, min: 5, max: 12, message: 'required: true, min: 5, max: 12'}]}
                 >
-                    <Input placeholder={'enter username'}/>
+                    <Input placeholder={'enter title'}/>
                 </Form.Item>
-                <Form.Item
-                    label="Ticket Description"
-                    name="description"
-                    rules={[{required: true, min: 10, message: 'At least 10 chars long'}]}
-                    validateTrigger={false}
+                <Form.Item label="Ticket Description" name="description"
+                           rules={[{required: true, min: 10, message: 'At least 10 chars long'}]}
+                           validateTrigger={false}
                 >
                     <Input.TextArea placeholder={'At least 10 chars long'}/>
                 </Form.Item>
                 <Form.Item name="category" label="Pick Category"
                            rules={[{required: true, message: 'please select your role'}]}>
-                    <Select
-                        placeholder="Select a ticket category"
-                        allowClear
-                    >
+                    <Select placeholder="Select a ticket category" allowClear>
                         <Option value="BUGS_AND_ERRORS">Bugs And Errors</Option>
                         <Option value="FEATURE_REQUEST">Feature Request</Option>
                         <Option value="OTHER">Other</Option>
@@ -63,10 +57,7 @@ function SubmitTicketForm() {
 
                 <Form.Item name="priority" label="Chose a Priority"
                            rules={[{required: true, message: 'please select your role'}]}>
-                    <Select
-                        placeholder="Select a ticket priority"
-                        allowClear
-                    >
+                    <Select placeholder="Select a ticket priority" allowClear>
                         <Option value="LOW">Low</Option>
                         <Option value="MEDIUM">Medium</Option>
                         <Option value="HIGH">High</Option>
@@ -75,14 +66,7 @@ function SubmitTicketForm() {
                 </Form.Item>
 
                 <Form.Item>
-                    <Button type="primary" htmlType="submit" block>
-                        Register
-                    </Button>
-                </Form.Item>
-                <Form.Item>
-                    <Button type="primary" htmlType="submit" block danger>
-                        All ready registered? Just sign in.
-                    </Button>
+                    <Button type="primary" htmlType="submit" block>Submit your ticket</Button>
                 </Form.Item>
             </Form>
         </React.Fragment>

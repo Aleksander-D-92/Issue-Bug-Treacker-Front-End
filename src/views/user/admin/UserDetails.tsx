5fe4b504bf05428fd56e928f155deb6d3835315f
Aleksander D <aleksander.dorkov@gmail.com>
2020-08-29 16:04:40 +0300

    Further improvements to Edit User for admins

--- src/views/user/admin/UserDetails.tsx
+++ src/views/user/admin/UserDetails.tsx
@@ -1,9 +1,9 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, MouseEvent} from "react";
 import axios from "axios";
 import {useSelector} from 'react-redux'
 import {useParams} from 'react-router-dom'
 import {ReduxState} from "../../../configuration/redux/reduxStrore";
-import {Button, Card, Col, Descriptions, Divider, Row, Select} from "antd";
+import {Button, Card, Col, Descriptions, Divider, Form, Input, Row, Select} from "antd";
 import {DownloadOutlined} from '@ant-design/icons';
 import {formatDate} from "../../shared/functions";
 import {AuthorityViewModel, UserViewModel} from "../../shared/Interfaces";
@@ -14,8 +14,8 @@ const {Option} = Select;
 function UserDetails() {
     const {userId} = useParams();
     const [user, setUser] = useState<UserViewModel>();
+    const [accountNonLocked, setAccountNonLocked] = useState<boolean>()
     const [authorities, setAuthorities] = useState<AuthorityViewModel[]>();
-    const [selectedAuthorityId, setSelectedAuthorityId] = useState<number>();
     const reduxState = useSelector((state: ReduxState) => state);
 
     useEffect(() => {
@@ -25,6 +25,7 @@ function UserDetails() {
             }
         }).then((e) => {
             setUser(e.data[0])
+            setAccountNonLocked(e.data[0].accountNonLocked)
         });
         axios.get('/authorities/all', {
             headers: {
@@ -35,8 +36,8 @@ function UserDetails() {
         })
     }, [])
 
-    function changeAuthority() {
-        axios.put(`/admins/authority/${userId}`, {authorityId: selectedAuthorityId}, {
+    function changeAuthority(form: any) {
+        axios.put(`/admins/user-authority?userId=${userId}&authorityId=${form.authority}`, {}, {
             headers: {
                 Authorization: reduxState.userDetails.authorizationHeader
             }
@@ -45,8 +46,18 @@ function UserDetails() {
         });
     }
 
-    function lockAccount() {
-        axios.put(`/admins/lock-account/${userId}`).then((e) => {
+    function lockAccount(e: MouseEvent<HTMLButtonElement>) {
+        let name = e.currentTarget.name;
+        if (name === 'lock') {
+            setAccountNonLocked(false);
+        } else {
+            setAccountNonLocked(true)
+        }
+        axios.put(`/admins/user-account-lock?action=${name}&userId=${userId}`, {}, {
+            headers: {
+                Authorization: reduxState.userDetails.authorizationHeader
+            }
+        }).then((e) => {
             console.log(e);
         });
     }
@@ -64,26 +75,45 @@ function UserDetails() {
                         <Descriptions.Item label="Authority level"
                                            span={2}>{user?.authority.authorityLevel}</Descriptions.Item>
                         <Descriptions.Item label="Is account Locked"
-                                           span={3}>{user?.accountNonLocked ? 'Not locked' : 'Locked'}</Descriptions.Item>
+                                           span={3}>{accountNonLocked ? 'Non locked' : 'Locked'}</Descriptions.Item>
                     </Descriptions>
                     <Divider>Change authority</Divider>
-                    <Select defaultValue={1} allowClear
-                            onChange={(value: number) => setSelectedAuthorityId(value)} style={{width: 400}}>
-                        {authorities?.map((authority) => {
-                            return <Option key={authority.id} value={authority.id}>{authority.authority} =
-                                Level {authority.authorityLevel}</Option>
-                        })}
-                    </Select>
-                    <Button type="primary" icon={<DownloadOutlined/>} size={'large'} onClick={changeAuthority}
-                            block={true}
-                            className={'mt-2'}>
-                        Change authority
-                    </Button>
-                    <Divider>Lock account</Divider>
+                    <Form
+                        name="normal_login"
+                        className="login-form"
+                        layout={'vertical'}
+                        onFinish={changeAuthority}>
+                        <Form.Item
+                            label="Authority"
+                            name="authority"
+                            rules={[{required: true, message: 'Must select at least one'}]}>
+                            <Select defaultValue={1} allowClear style={{width: 400}}>
+                                {authorities?.map((authority) => {
+                                    return <Option key={authority.id} value={authority.id}>{authority.authority} =
+                                        Level {authority.authorityLevel}</Option>
+                                })}
+                            </Select>
+                        </Form.Item>
+                        <Button type="primary" icon={<DownloadOutlined/>} size={'large'}
+                                block={true} htmlType={'submit'}
+                                className="login-form-button">
+                            Change authority
+                        </Button>
+                    </Form>
+                    <Divider>Set account lock</Divider>
                     <Button type={'primary'} danger={true} icon={<DownloadOutlined/>} size={'large'}
                             onClick={lockAccount}
-                            block={true} className={'mt-2'}>
-                        Lock/Ban account
+                            block={true} className={'mt-2'}
+                            name={'lock'}
+                            disabled={!accountNonLocked}>
+                        Lock account
+                    </Button>
+                    <Button type={'primary'} icon={<DownloadOutlined/>} size={'large'}
+                            onClick={lockAccount}
+                            block={true} className={'mt-2'}
+                            name={'unlock'}
+                            disabled={!!accountNonLocked}>
+                        Unlock account
                     </Button>
                 </Card>
             </Col>

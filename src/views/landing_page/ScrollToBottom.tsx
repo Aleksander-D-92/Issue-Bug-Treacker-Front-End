7467a0117c016d3e434e4fa5a87fa03b9a9eb88e
Aleksander D <aleksander.dorkov@gmail.com>
2020-09-13 16:34:19 +0300

    Added scrolls to landing page

---
+++ src/views/landing_page/ScrollToBottom.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './scrolltobottom.css'
+import {VerticalAlignBottomOutlined} from '@ant-design/icons';
+
+function ScrollToBottom() {
+    return (
+        <>
+            <a href={"#12232"}
+               className={'scroll bottom'}>
+                <VerticalAlignBottomOutlined style={{fontSize: '3rem'}}/>
+            </a>
+        </>
+    )
+}
+
+export {ScrollToBottom}
